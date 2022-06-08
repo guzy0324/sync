7381de155281659706cdb1ac40692b69c4db5767
Taco <1141374791@qq.com>
2022-06-09 00:24:49 +0800

    Thu Jun  9 00:24:49     2022

--- upload.sh
+++ upload.sh
@@ -3,10 +3,10 @@ export COMMIT_MESSAGE=`date`
 export HOME="C:/Users/guzy0"
 export NVIM_PATH="C:/msys64/Neovim/share/nvim"
 export WT_PATH="C:/Users/guzy0/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState"
-export PWSH_PATH="C:/Users/guzy0/Documents/PowerShell/Microsoft.PowerShell_profile.ps1"
+export PWSH_PATH="C:/Users/guzy0/Documents/PowerShell"
 
-pacman -Qqen > ./msys2/pkglist.txt
-cp "${NVIM_PATH}/sysinit.vim" ./nvim
+# pacman -Qqen > ./msys2/pkglist.txt
+# cp "${NVIM_PATH}/sysinit.vim" ./nvim
 cp "${WT_PATH}/settings.json" ./wt
 cp "${PWSH_PATH}/Microsoft.PowerShell_profile.ps1" ./pwsh
 
