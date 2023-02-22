49795ad3422709d4f03f51d84577d7a28463736f
Taco <1141374791@qq.com>
2023-02-22 12:03:44 +0800

    Wed Feb 22 12:03:43     2023

--- upload.sh
+++ upload.sh
@@ -1,12 +1,12 @@
 export COMMIT_MESSAGE=`date`
 # https://stackoverflow.com/questions/6770044/windows-scheduled-task-to-git-push-to-github
 export HOME="C:/Users/guzy0"
-export NVIM_PATH="C:/msys64/Neovim/share/nvim"
+export NVIM_PATH="C:/Program Files/Neovim/share/nvim"
 export WT_PATH="C:/Users/guzy0/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState"
 export PWSH_PATH="C:/Users/guzy0/Documents/PowerShell"
 
 # pacman -Qqen > ./msys2/pkglist.txt
-# cp "${NVIM_PATH}/sysinit.vim" ./nvim
+cp "${NVIM_PATH}/sysinit.vim" ./nvim
 cp "${WT_PATH}/settings.json" ./wt
 cp "${PWSH_PATH}/Microsoft.PowerShell_profile.ps1" ./pwsh
 
