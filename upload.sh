6fe8fc2a416f5ea45ad891c92979f06cd48cd5ea
Taco <1141374791@qq.com>
2025-01-10 11:04:24 +0800

    2025年01月10日 11:04:21

--- upload.sh
+++ upload.sh
@@ -4,11 +4,13 @@ export HOME="C:/Users/guzy0"
 export NVIM_PATH="C:/Program Files/Neovim/share/nvim"
 export WT_PATH="C:/Users/guzy0/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState"
 export PWSH_PATH="C:/Users/guzy0/Documents/PowerShell"
+export KEYBOARD_CONFIG_PATH="C:/键盘/config.pro"
 
 # pacman -Qqen > ./msys2/pkglist.txt
 cp "${NVIM_PATH}/sysinit.vim" ./nvim
 cp "${WT_PATH}/settings.json" ./wt
 cp "${PWSH_PATH}/Microsoft.PowerShell_profile.ps1" ./pwsh
+cp "${KEYBOARD_CONFIG_PATH}" ./keyboard
 
 git config --global user.name "Taco"
 git config --global user.email "1141374791@qq.com"
